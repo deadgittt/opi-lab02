b2d275969d3e0c979383ec9b4987ecc2937c345e
intelinos <intelinos@gmail.com>
2024-04-26 17:08:34 +0300

    r1

---
+++ main_prog.c
@@ -0,0 +1,12 @@
+#include "vector.h"
+#include <inttypes.h>
+#include <stdio.h>
+
+int main() {
+    struct vector* vec = vector_create();
+    for (size_t i = 0; i < 100; i++) {
+        vector_push_back(vec, i * i);
+    }
+    vector_print(vec, stdout);
+    return 0;
+}
