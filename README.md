faf506c25db5f81145486fb1b013cc231ad56bce
Yaroslav <gainer495@gmail.com>
2024-04-26 18:04:38 +0300

    r14

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # opi-lab02
-Hi! There is our work on OPI's 2nd lab!
-
+Hi! There is our work on OPI's 2nd lab!!!!!!!!!!!!!!
 Authors: Gaiderov Yaroslav and Strozenko Konstantin
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "MyClass.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    cout << "Hello, world!" << endl;
     return 0;
 }
