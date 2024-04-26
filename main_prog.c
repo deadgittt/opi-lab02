df79691adf25391a1e31a0e00d3d8433cc4b2208
Yaroslav <gainer495@gmail.com>
2024-04-26 17:26:55 +0300

    r5

--- main_prog.c
+++ main_prog.c
@@ -4,9 +4,10 @@
 
 int main() {
     struct vector* vec = vector_create();
-    for (size_t i = 0; i < 100; i++) {
+    for (size_t i = 0; i < 100; ++i) {
         vector_push_back(vec, i * i);
     }
     vector_print(vec, stdout);
+    printf("hihihiha");
     return 0;
 }
